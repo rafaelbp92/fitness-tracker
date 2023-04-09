7bafee733b21e819329ac71df38ca29780fd8a38
Rafael Pantoja <rafaelbp92@gmail.com>
2023-04-09 20:37:54 +0100

    Replacing conditional to show on sidenav

--- src/app/navigation/sidenav-list/sidenav-list.component.ts
+++ src/app/navigation/sidenav-list/sidenav-list.component.ts
@@ -1,15 +1,31 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, OnDestroy, OnInit, Output } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { AuthService } from 'src/app/auth/services/auth.service';
 
 @Component({
   selector: 'app-sidenav-list',
   templateUrl: './sidenav-list.component.html',
   styleUrls: ['./sidenav-list.component.css']
 })
-export class SidenavListComponent {
-  @Output()
-  sideNavToggle = new EventEmitter<void>();
+export class SidenavListComponent implements OnInit, OnDestroy {
+  @Output() sideNavToggle = new EventEmitter<void>();
+
+  isAuth = false;
+  authSubscription: Subscription | undefined;
+
+  constructor(private authService: AuthService) { }
+
+  ngOnInit() {
+    this.authSubscription = this.authService.authChange.subscribe(authStatus => {
+      this.isAuth = authStatus;
+    });
+  }
   
   onToggleSidenav() {
     this.sideNavToggle.emit();
+  }
+
+  ngOnDestroy() {
+    this.authSubscription?.unsubscribe();
   }
 }
