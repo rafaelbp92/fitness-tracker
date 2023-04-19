17576f69b6c9f5abf3f98a6133610958637f1869
Rafael Pantoja <rafaelbp92@gmail.com>
2023-04-19 21:24:30 +0100

    Moving fetch logic to service

--- src/app/training/new-training/new-training.component.ts
+++ src/app/training/new-training/new-training.component.ts
@@ -1,28 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { TrainingService } from '../training.service';
 import { Exercise } from '../exercise.model';
 import { NgForm } from '@angular/forms';
-import { Firestore, collection,collectionData } from '@angular/fire/firestore';
-import { Observable } from 'rxjs';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-new-training',
   templateUrl: './new-training.component.html',
   styleUrls: ['./new-training.component.css']
 })
-export class NewTrainingComponent implements OnInit {
-  exercises: Observable<any> | undefined;
-  constructor(private trainingService: TrainingService,
-    private db: Firestore){}
+export class NewTrainingComponent implements OnInit, OnDestroy {
+  exercises: Exercise[] = [];
+  exercisesSubscription: Subscription | undefined;
+  constructor(private trainingService: TrainingService){}
 
   ngOnInit(): void {
-    //this.exercises = this.trainingService.getAvailableExercises(); 
-
-    const availableExercisesCollection = collection(this.db, 'avilable_exercises');
-    this.exercises = collectionData(availableExercisesCollection, {idField: 'id'});
+    //this.exercises = this.trainingService.getAvailableExercises();
+    this.exercisesSubscription = this.trainingService.exercisesChanged.subscribe(exercises => {
+      if (exercises) {
+        this.exercises = exercises;
+      }
+    }); 
+    this.trainingService.getAvailableExercises();
   }
 
   onStartTraining(form: NgForm) {
     this.trainingService.startExercise(form.value.exercise);
+  }
+
+  ngOnDestroy(): void {
+    this.exercisesSubscription?.unsubscribe();
   }
 }
--- src/app/training/training.service.ts
+++ src/app/training/training.service.ts
@@ -1,20 +1,25 @@
 import { Subject } from 'rxjs';
 
 import { Exercise } from './exercise.model';
+import { Injectable } from '@angular/core';
+import { Firestore, collection, collectionData } from '@angular/fire/firestore';
 
+@Injectable()
 export class TrainingService {
   exerciseChanged = new Subject<Exercise | null>();
-  private availableExercises: Exercise[] = [
-    { id: 'crunches', name: 'Crunches', duration: 30, calories: 8 },
-    { id: 'touch-toes', name: 'Touch Toes', duration: 180, calories: 15 },
-    { id: 'side-lunges', name: 'Side Lunges', duration: 120, calories: 18 },
-    { id: 'burpees', name: 'Burpees', duration: 60, calories: 8 }
-  ];
+  exercisesChanged = new Subject<Exercise[] | null>();
+  private availableExercises: Exercise[] = [];
   private runningExercise: Exercise | undefined;
   private exercises: Exercise[] = [];
 
+  constructor(private db: Firestore){}
+
   getAvailableExercises() {
-    return this.availableExercises.slice();
+    const availableExercisesCollection = collection(this.db, 'avilable_exercises');
+    collectionData(availableExercisesCollection, {idField: 'id'}).subscribe((exercises: any) => {
+      this.availableExercises = exercises;
+      this.exercisesChanged.next([...this.availableExercises]);
+    })
   }
 
   startExercise(selectedId: string) {
