76fcec4e9a5c7f2112167947aae7a5415f0a53f5
Rafael Pantoja <rafaelbp92@gmail.com>
2024-01-03 17:32:20 +0000

    Removing firebase config from env files

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,11 +1,6 @@
+import {firebase} from "firebase-config";
+
 export const environment = {
     production: false,
-    firebase: {
-        apiKey: "AIzaSyBJ9LUFvNE_uJG0woPuHnxph0sjG14xq6s",
-        authDomain: "ng-fitness-tracker-d4f45.firebaseapp.com",
-        projectId: "ng-fitness-tracker-d4f45",
-        storageBucket: "ng-fitness-tracker-d4f45.appspot.com",
-        messagingSenderId: "43307655276",
-        appId: "1:43307655276:web:c89d67002a2a015675f849"
-    }
+    firebase
 }
